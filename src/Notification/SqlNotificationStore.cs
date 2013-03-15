aeaf304bce83f473953eac218d2f2582bc11e696
Gernot Rumpold <gernot.rumpold@gmail.com>
2013-03-15 16:11:02 +0100

    Moved Notification project to proper location

---
+++ src/Notification/SqlNotificationStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dragon.Interfaces.Notifications;
+
+namespace Dragon.Notification
+{
+    public class SqlNotificationStore : INotificationStore
+    {
+    }
+}
