965d83f2b537d7860a7cee8ec79722f0169dfe38
Johannes Binder <j.binder.x@gmail.com>
2013-04-12 16:34:39 +0200

    WebNotifiable skeleton added

---
+++ src/Notification/WebNotificationDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using Dragon.Interfaces.Notifications;
+
+namespace Dragon.Notification
+{
+    class WebNotificationDispatcher : INotificationDispatcher<IWebNotifiable>
+    {
+        public void Dispatch(IWebNotifiable notifiable, INotification notification)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
---
+++ src/NotificationTest/WebNotificationDispatcherTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NotificationTest
+{
+    [TestClass]
+    public class WebNotificationDispatcherTest
+    {
+        [TestMethod]
+        public void DispatchShouldInvokeWebServiceCall()
+        {
+            Assert.Fail();
+        }
+    }
+}
