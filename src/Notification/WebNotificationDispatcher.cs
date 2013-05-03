742c6e4bad55f490a409e3cb3c36eb3ffceae229
Johannes Binder <j.binder.x@gmail.com>
2013-05-03 14:23:20 +0200

    WebNotification demo implementation

--- src/Notification/WebNotificationDispatcher.cs
+++ src/Notification/WebNotificationDispatcher.cs
@@ -1,12 +1,35 @@
 ﻿using Dragon.Interfaces.Notifications;
+using Microsoft.AspNet.SignalR;
 
 namespace Dragon.Notification
 {
-    class WebNotificationDispatcher : INotificationDispatcher<IWebNotifiable>
+    // TODO: test and select proper data source
+    // TODO: verify that route has been set, else throw exception
+    public class WebNotificationDispatcher : INotificationDispatcher<IWebNotifiable>
     {
+        private readonly ITemplateService _templateService;
+        private readonly ILocalizedDataSource _dataSource;
+        private readonly Chat _chat = new Chat();
+
+        public class Chat : Hub
+        {
+            public void Send(string message)
+            {
+                Clients.All.addMessage(message);
+            }
+        }
+
+        public WebNotificationDispatcher(ITemplateService templateService, ILocalizedDataSource dataSource)
+        {
+            _templateService = templateService;
+            _dataSource = dataSource;
+        }
+
         public void Dispatch(IWebNotifiable notifiable, INotification notification)
         {
-            throw new System.NotImplementedException();
+            var bodyTemplate = _dataSource.GetContent(notification.TypeKey, notification.LanguageCode);
+            var body = _templateService.Parse(bodyTemplate, notification.Parameter);
+            _chat.Send(body);
         }
     }
 
