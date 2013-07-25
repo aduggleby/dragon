8bf7e47fd15d05a396172ef2a92d72c5f3260ab8
Johannes Binder <j.binder.x@gmail.com>
2013-07-25 16:42:13 +0200

    Using ajax form for the NotificationTest

--- src/Demo/Controllers/NotificationTestController.cs
+++ src/Demo/Controllers/NotificationTestController.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
 using System.Web.Mvc;
 using Dragon.Interfaces;
+using Dragon.Interfaces.Notifications;
 using Dragon.Notification;
 using StructureMap;
 
@@ -14,10 +17,75 @@ namespace Demo.Controllers
         }
 
         [HttpPost]
-        public ActionResult EMailTest(string email, string subject, string key)
+        public ActionResult Index(string email, string subject, string key)
         {
-            // TODO: inject this stuff
-            var notifiable = new EmailNotifiable { PrimaryEmailAddress = email, UseHTMLEmail = false };
+            var action = Request.Form["action"];
+            switch (action)
+            {
+                case "EnqueueMail":
+                    return EnqueueMail(email, subject, key);
+                case "BatchSendMails":
+                    return BatchSendMails(email, subject);
+                case "SendMail":
+                    return SendMail(email, subject, key);
+            }
+            throw new HttpException(400, "Action not found");
+        }
+
+        private ActionResult BatchSendMails(string email, string subject)
+        {
+            var notifiable = CreateNotifiable(email);
+            var dispatcher = CreateOrGetBatchEmailNotificationDispatcher();
+            dispatcher.DispatchAll(notifiable, subject);
+            return Json(""); // TODO: return PartialView("NotificationResultPartial");
+        }
+
+        private ActionResult EnqueueMail(string email, string subject, string key)
+        {
+            var notifiable = CreateNotifiable(email);
+            var notification = CreateNotification(email, subject, key);
+            var dispatcher = CreateOrGetBatchEmailNotificationDispatcher();
+            dispatcher.Dispatch(notifiable, notification);
+            return Json(""); // TODO: return PartialView("NotificationResultPartial");
+         }
+
+        private IBatchNotificationDispatcher<IEmailNotifiable> CreateOrGetBatchEmailNotificationDispatcher()
+        {
+            if (Session["EmailBatchNotificationDispatcher"] == null)
+            {
+                Session["EmailBatchNotificationDispatcher"] = new EmailBatchNotificationDispatcher(
+                    new NetEmailService {Configuration = ObjectFactory.GetInstance<IConfiguration>()},
+                    new StringTemplateTemplateService(),
+                    new FileSystemLocalizedDataSource(HttpContext.Server.MapPath("~/Resources/templates"), "txt"));
+            }
+           return (IBatchNotificationDispatcher<IEmailNotifiable>) Session["EmailBatchNotificationDispatcher"];
+        }
+
+        private ActionResult SendMail(string email, string subject, string key)
+        {
+            var notifiable = CreateNotifiable(email);
+            var notification = CreateNotification(email, subject, key);
+            var dispatcher = CreateEmailNotificationDispatcher();
+            dispatcher.Dispatch(notifiable, notification);
+
+            var webNotifiable = new WebNotifiable();
+
+            var webDispatcher = new WebNotificationDispatcher(
+                new StringTemplateTemplateService(),
+                new FileSystemLocalizedDataSource(HttpContext.Server.MapPath("~/Resources/templates"), "txt"));
+            webDispatcher.Dispatch(webNotifiable, notification);
+
+            return Json(""); // TODO: return PartialView("NotificationResultPartial");
+        }
+
+        private static EmailNotifiable CreateNotifiable(string email)
+        {
+            var notifiable = new EmailNotifiable {PrimaryEmailAddress = email, UseHTMLEmail = false};
+            return notifiable;
+        }
+
+        private static Notification CreateNotification(string email, string subject, string key)
+        {
             var notification = new Notification
             {
                 LanguageCode = "de",
@@ -25,11 +93,17 @@ namespace Demo.Controllers
                 Subject = subject,
                 TypeKey = key,
             };
-            var dispatcher = new EmailNotificationDispatcher(
-                new NetEmailService {Configuration = ObjectFactory.GetInstance<IConfiguration>()}, new StringTemplateTemplateService(), new FileSystemLocalizedDataSource(HttpContext.Server.MapPath("~/Resources/templates"), "txt")); // TODO: move to config
-            dispatcher.Dispatch(notifiable, notification);
-            return View();
+            return notification;
         }
 
+        private EmailNotificationDispatcher CreateEmailNotificationDispatcher()
+        {
+            var dispatcher = new EmailNotificationDispatcher(
+                new NetEmailService {Configuration = ObjectFactory.GetInstance<IConfiguration>()},
+                new StringTemplateTemplateService(),
+                new FileSystemLocalizedDataSource(HttpContext.Server.MapPath("~/Resources/templates"), "txt"));
+            // TODO: move to config
+            return dispatcher;
+        }
     }
 }
