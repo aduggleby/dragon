82329ae1e5b92af8bda6a146bdd06f7c22ebcb89
Alex Duggleby <ad@dualconsult.com>
2013-11-14 09:57:11 +0100

    Add: Intercepting projection base (in order not to have repository if not required)

--- src/CPR/Impl/Projections/TargetProjectionBase.cs
+++ src/CPR/Impl/Projections/TargetProjectionBase.cs
@@ -9,7 +9,7 @@ using StructureMap;
 
 namespace Dragon.CPR.Impl.Projections
 {
-    public abstract class TargetProjectionBase<TSrc, TDest> : ProjectionBase<TSrc>
+    public abstract class TargetProjectionBase<TSrc, TDest> : InterceptingProjectionBase<TSrc, TDest>
         where TSrc : class
         where TDest : class
     {
@@ -18,43 +18,11 @@ namespace Dragon.CPR.Impl.Projections
         public IPermissionStore PermissionStore { get; set; }
         public DragonContext Ctx { get; set; }
 
-        private Dictionary<Type, List<Action<TDest>>> m_interceptors;
 
         public IContainer Container { get; set; }
 
-        public TargetProjectionBase()
-        {   
-            // TODO: Can't find a solution for this which is cleaner right now ...
-            Container = ObjectFactory.Container;
+     
 
-            m_interceptors = new Dictionary<Type, List<Action<TDest>>>();
-            var t = typeof(TDest);
-            while (t != null)
-            {
-                var genType = typeof(IInterceptor<>).MakeGenericType(t);
-                var l = new List<Action<TDest>>();
-                foreach (var theinstance in Container.GetAllInstances(genType))
-                {
-                    var instance = theinstance;
-                    Action<object> f = o =>
-                                       instance.GetType()
-                                               .GetMethod("Intercept")
-                                               .Invoke(instance, new object[] { o });
-                    l.Add(f);
-                }
-                m_interceptors.Add(t, l);
-                t = t.BaseType;
-            }
-        }
-
-        protected void Intercept(TDest dest)
-        {
-            foreach (var t in m_interceptors.Keys)
-            {
-                var list = m_interceptors[t];
-                list.ForEach(x => x(dest));
-            }
-        }
-
+     
     }
 }
