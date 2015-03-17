87649bdae8fa2c622c7e9d2c53ba32465bede272
Alex Duggleby <ad@dualconsult.com>
2015-03-17 15:11:34 +0100

    Convert the awesome nupkg to basic boring nuspecs to please Johannes and let him continue working.

---
+++ nuspec/BuildAll.bat
@@ -0,0 +1,5 @@
+c:\tools\nuget\nuget.exe pack Dragon.CPR.nuspec
+c:\tools\nuget\nuget.exe pack Dragon.Data.nuspec
+c:\tools\nuget\nuget.exe pack Dragon.Files.nuspec
+c:\tools\nuget\nuget.exe pack Dragon.Files.MVC.nuspec
+c:\tools\nuget\nuget.exe pack Dragon.Files.S3.nuspec
