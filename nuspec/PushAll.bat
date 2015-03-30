86504797cfc899f92aaa6bf28ddd0881d4eaae25
Alex Duggleby <ad@dualconsult.com>
2015-03-30 15:51:00 +0200

    Pushing nuspecs

---
+++ nuspec/PushAll.bat
@@ -0,0 +1,7 @@
+c:\tools\nuget\nuget.exe push Dragon.CPR.1.0.0.nupkg
+c:\tools\nuget\nuget.exe push Dragon.Data.1.0.0.nupkg
+c:\tools\nuget\nuget.exe push Dragon.Files.1.2.0.nupkg
+c:\tools\nuget\nuget.exe push Dragon.Files.MVC.1.2.0.nupkg
+c:\tools\nuget\nuget.exe push Dragon.Files.S3.1.2.0.nupkg
+c:\tools\nuget\nuget.exe push Dragon.Security.Hmac.Core.1.0.0.nupkg
+c:\tools\nuget\nuget.exe push Dragon.Security.Hmac.Module.1.0.0.nupkg
