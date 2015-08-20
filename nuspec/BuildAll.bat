ed2592afa9c6fed68da18230e368c92712b72eea
Alex Duggleby <ad@dualconsult.com>
2015-08-20 15:04:12 +0200

    Missing nuspecs

--- nuspec/BuildAll.bat
+++ nuspec/BuildAll.bat
@@ -1,5 +1,6 @@
 c:\tools\nuget\nuget.exe pack Dragon.CPR.nuspec
 c:\tools\nuget\nuget.exe pack Dragon.Data.nuspec
+c:\tools\nuget\nuget.exe pack Dragon.Mail.nuspec
 c:\tools\nuget\nuget.exe pack Dragon.Files.nuspec
 c:\tools\nuget\nuget.exe pack Dragon.Files.MVC.nuspec
 c:\tools\nuget\nuget.exe pack Dragon.Files.S3.nuspec
--- nuspec/PushAll.bat
+++ nuspec/PushAll.bat
@@ -1,5 +1,5 @@
 c:\tools\nuget\nuget.exe push Dragon.CPR.1.0.0.nupkg
-c:\tools\nuget\nuget.exe push Dragon.Data.1.0.0.nupkg
+c:\tools\nuget\nuget.exe push Dragon.Data.1.1.0.nupkg
 c:\tools\nuget\nuget.exe push Dragon.Files.1.2.0.nupkg
 c:\tools\nuget\nuget.exe push Dragon.Files.MVC.1.2.0.nupkg
 c:\tools\nuget\nuget.exe push Dragon.Files.S3.1.2.0.nupkg
