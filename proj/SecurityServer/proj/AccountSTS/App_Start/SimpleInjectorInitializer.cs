019aafee8c1d8453f1ed14535e29c18a98188ec3
Johannes Binder <j.binder.x@gmail.com>
2016-02-15 15:40:49 +0100

    Redis made optional

--- proj/SecurityServer/proj/AccountSTS/App_Start/SimpleInjectorInitializer.cs
+++ proj/SecurityServer/proj/AccountSTS/App_Start/SimpleInjectorInitializer.cs
@@ -25,6 +25,8 @@ namespace Dragon.SecurityServer.AccountSTS
 {
     public static class SimpleInjectorInitializer
     {
+        private const string RedisConnectionStringName = "Redis";
+
         public static Container Initialize(IAppBuilder app)
         {
             var container = GetInitializeContainer(app);
@@ -62,14 +64,18 @@ namespace Dragon.SecurityServer.AccountSTS
  
             container.RegisterPerWebRequest<IDragonUserStore<AppMember>>(() =>
             {
-                var connectionMultiplexer = ConnectionMultiplexer.Connect(WebConfigurationManager.ConnectionStrings["Redis"].ConnectionString);
-                connectionMultiplexer.PreserveAsyncOrder = false;
-                return new ChainedIdentity.Stores.UserStore<AppMember>(new List<IDragonUserStore<AppMember>>
+                var dragonUserStores = new List<IDragonUserStore<AppMember>>
                 {
-                    new Identity.Redis.UserStore<AppMember>(new RedisUserStore<Identity.Redis.IdentityUser>(connectionMultiplexer), connectionMultiplexer),
                     new UserStore<AppMember>(new Repository<AppMember>(), null, new Repository<IdentityUserLogin>(),
                         new Repository<IdentityService>())
-                });
+                };
+                if (WebConfigurationManager.ConnectionStrings[RedisConnectionStringName] != null)
+                {
+                    var connectionMultiplexer = ConnectionMultiplexer.Connect(WebConfigurationManager.ConnectionStrings[RedisConnectionStringName].ConnectionString);
+                    connectionMultiplexer.PreserveAsyncOrder = false;
+                    dragonUserStores.Insert(0, new Identity.Redis.UserStore<AppMember>(new RedisUserStore<Identity.Redis.IdentityUser>(connectionMultiplexer), connectionMultiplexer));
+                }
+                return new ChainedIdentity.Stores.UserStore<AppMember>(dragonUserStores);
             });
 
             container.RegisterPerWebRequest(() => ApplicationUserManager.Create(container, Startup.DataProtectionProvider));
