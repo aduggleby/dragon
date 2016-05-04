47aa3648e66a24a157b3fa82e1cbfff8816f6469
Alex Duggleby <alex@whataventure.com>
2016-05-04 13:23:42 +0200

    CPRX

---
+++ proj/CPRX/src/Dragon.CPRX/CommandBaseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dragon.CPRX
+{
+    public static class CommandBaseExtensions
+    {
+        public static void Execute(this CommandBase cmd)
+        {
+
+        }
+    }
+}
---
+++ proj/CPRX/src/Dragon.CPRX/CommandExecutionResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dragon.CPRX
+{
+    public class CommandExecutionResult
+    {
+        public CommandExecutionResult()
+        {
+            PipelineResults = new List<CommandPipelineResult>();
+        }
+
+        public bool Success
+        {
+            get
+            {
+                return PipelineResults.Any(x => !x.Success);
+            }
+        }
+
+        public IList<CommandPipelineResult> PipelineResults { get; set; }
+    }
+}
